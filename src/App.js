b2fee3e9e52461cf548fe1ef3ebd88c663973ee5
Ryan Pickett <piratematic@gmail.com>
2019-09-17 15:05:36 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Choice from "./components/Choice";
+import { Container, Header, Button, } from "semantic-ui-react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component{
+  state = {
+    choices: [
+      { id: 1, name: "Rock", },
+      { id: 2, name: "Paper", },
+      { id: 3, name: "Scissors", },
+    ]
+  };
+  
+  render() {
+    return(
+      <Container textAlign="center">
+        <Header as="h1" color="blue">Rock Paper Scissors</Header>
+        <br />
+        <br />
+        <Choice 
+          choice={this.state.choice}
+        />
+        {/* <Button icon="hand rock" onClick={this.rock} />
+        <Button icon="hand paper" onClick={this.paper} />
+        <Button icon="hand scissors" onClick={this.scissors} /> */}
+      </Container>
+    )
+  };
+};
+
+const styles = {
+  container: {
+    marginTop: "25px",
+  },
+};
 
 export default App;
---
+++ src/components/Choice.js
@@ -0,0 +1,8 @@
+import React from "react"
+import { Button, Card, } from "semantic-ui-react"
+
+class Choice extends React.Component {
+
+};
+
+export default Choice;
